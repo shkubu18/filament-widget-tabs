9d7e0eda54fdf70dc0668346010e481926412155
StfnSl <stefan.schoelzel@gmail.com>
2025-07-15 18:38:18 +0200

    Extension of the views so that the number of columns can be generated more dynamically and after breakpoint, change of the return of getWidgetsPerRow, admit an array or an integer can  be returned.

--- resources/views/components/widget-tabs/index.blade.php
+++ resources/views/components/widget-tabs/index.blade.php
@@ -1,15 +1,59 @@
 @php
     $widgetsPerRow = $this->getWidgetsPerRow();
+    if (!is_array($widgetsPerRow)) {
+        $widgetsPerRow = [
+            'md' => $widgetsPerRow,
+        ];
+    }
+    $columns = collect([
+        ...[
+            'default' => 1,
+            'sm' => 2,
+            'md' => 3,
+            'lg' => null,
+            'xl' => null,
+            '2xl' => null,
+        ],
+        ...$widgetsPerRow,
+    ]);
+    $classes = $columns
+        ->filter()
+        ->keys()
+        ->map(
+            fn($key): string => str($key)
+                ->whenIs(
+                    'default',
+                    fn($string) => $string->replace($key, 'grid-cols-[--cols-'),
+                    fn($string) => $string->append(':grid-cols-[--cols-'),
+                )
+                ->append($key)
+                ->finish(']')
+                ->value(),
+        )
+        ->all();
+    $columns = $columns
+        ->mapWithKeys(
+            fn($value, $key): array => [
+                str($key)
+                    ->start('--cols-')
+                    ->append(': repeat(')
+                    ->append($value)
+                    ->finish(', minmax(0, 1fr))')
+                    ->value() => $value,
+            ],
+        )
+        ->filter()
+        ->all();
 @endphp
 
 <div
-    {{
-        $attributes
-            ->merge(['role' => 'tablist'])
-            ->class([
-                "fi-widget-tabs grid grid-cols-1 sm:grid-cols-2 md:grid-cols-$widgetsPerRow gap-4",
+    {{ $attributes
+        ->merge(['role' => 'tablist'])
+        ->class([
+            'fi-widget-tabs grid',
+            ...$classes,
+            'gap-4'
             ])
-    }}
->
+            ->style($columns) }}>
     {{ $slot }}
 </div>
